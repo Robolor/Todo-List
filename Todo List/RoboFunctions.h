438de333b594f628bed2c47d6580f0dfc05fc1f8
Robolor <robolor.official@gmail.com>
2024-04-18 06:07:59 -0400

    redesigned project to include header files for functions and class

---
+++ Todo List/RoboFunctions.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <vector>
+#include "Entry.h"
+using namespace std;
+
+
+void DisplayTODOnoclear(vector <Entry>& j);
+void Mainmenu(vector <Entry>& i);
+void DisplayTODO(vector <Entry>& j);
+void AddEntries(int i, vector <Entry>& j);
+void WaitForUser();
+void ClearScreen();
+
